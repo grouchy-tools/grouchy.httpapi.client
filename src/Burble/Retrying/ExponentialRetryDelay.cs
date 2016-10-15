f377a3dfe8e453b9b608b6f0df93554b015a30c4
acraven <andrew@epworth-consulting.co.uk>
2016-10-15 18:00:20 +0100

    Add retry delay scenarios

---
+++ src/Burble/Retrying/ExponentialRetryDelay.cs
@@ -0,0 +1,20 @@
+﻿namespace Burble.Retrying
+{
+   using System;
+   using Burble.Abstractions;
+
+   public class ExponentialRetryDelay : IRetryDelay
+   {
+      private readonly int _initialDelayMs;
+
+      public ExponentialRetryDelay(int initialDelayMs)
+      {
+         _initialDelayMs = initialDelayMs;
+      }
+
+      public int DelayMs(int retryAttempt)
+      {
+         return _initialDelayMs * (int)Math.Pow(2, retryAttempt - 1);
+      }
+   }
+}
---
+++ src/Burble/Retrying/LinearRetryDelay.cs
@@ -0,0 +1,19 @@
+﻿namespace Burble.Retrying
+{
+   using Burble.Abstractions;
+
+   public class LinearRetryDelay : IRetryDelay
+   {
+      private readonly int _delayMs;
+
+      public LinearRetryDelay(int delayMs)
+      {
+         _delayMs = delayMs;
+      }
+
+      public int DelayMs(int retryAttempt)
+      {
+         return _delayMs * retryAttempt;
+      }
+   }
+}
