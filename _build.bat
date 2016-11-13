d887da59f847e30df81ec2a9ddf0bbe0d1d3907b
acraven <andrew@epworth-consulting.co.uk>
2016-11-13 21:11:56 +0000

    Attempt to fail build on failing psake task

--- _build.bat
+++ _build.bat
@@ -1,5 +1,5 @@
 @echo off
 
-powershell "Import-Module %~dp0\build\psake.psm1 ; Invoke-Psake %~dp0\build\build.ps1 ; exit $LASTEXITCODE;"
+powershell "Import-Module %~dp0\build\psake.psm1; Invoke-Psake %~dp0\build\build.ps1; if ($psake.build_success -eq $false) { exit 1 } else { exit 0 }"
 
 pause
