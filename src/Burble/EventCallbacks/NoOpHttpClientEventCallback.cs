474709d78bbaf13ba336a04f9beb037d6818c868
acraven <andrew@epworth-consulting.co.uk>
2017-02-24 21:02:52 +0000

    Move json and no-op event callbacks from bivouac

---
+++ src/Burble/EventCallbacks/NoOpHttpClientEventCallback.cs
@@ -0,0 +1,11 @@
+﻿namespace Burble.EventCallbacks
+{
+   using Burble.Abstractions;
+
+   public class NoOpHttpClientEventCallback : IHttpClientEventCallback
+   {
+      public void Invoke(IHttpClientEvent @event)
+      {
+      }
+   }
+}
